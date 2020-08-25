e94ba6e03807191c70c3ddd7bfb903d4586e9898
Aleksandr <nayteruz@gmail.com>
2020-08-25 11:08:48 +0700

    Update toggle.staff.blocks.js

--- toggle.staff.blocks.js
+++ toggle.staff.blocks.js
@@ -1,7 +1,17 @@
 if ($.fn.jquery){
     console.log('script is ready');
     let is_state = 'staff_user';
-    let hide_blocks_classes = ['.version-json-controls', '.staffUser', '.staff.with-arrow-thin'];
+    let hide_blocks_classes = ['.version-json-controls',
+                               '.staffUser',
+                               '.staff.with-arrow-thin',
+                               '.data-list-item.system',
+                               'li.add-menu-wrap',
+                               '.item-col.query',
+                               '.item-col.alias',
+                               '.item-col .edit',
+                               '.tabs-header li a[href="#tab-page_boilerplate"]',
+                               '.client-hidden'
+                              ];
         hide_blocks_classes = hide_blocks_classes.join(', ');
     $(document).on('keydown', function(e){
         if ( e.ctrlKey && e.keyCode == 81 ) {
